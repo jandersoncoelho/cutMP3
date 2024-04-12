a5ee34991160883ccdd235aeb784715aaa06533e
Janderson de Almeida <jandersoncoelho@gmail.com>
2024-04-12 20:47:39 -0300

    Criando barra de progresso e diretórios
    Os diretórios terão o mesmo nome do arquivo de áudio fornecido.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from pydub import AudioSegment
+from tqdm import tqdm
+import os
 
 
 # Função para dividir o áudio em partes e salvar como MP3
@@ -9,21 +11,35 @@ def dividir_audio(arquivo, duracao_corte):
     # Calcular o número de cortes necessários
     duracao_total_ms = len(audio)
     duracao_corte_ms = duracao_corte * 60 * 1000  # Convertendo minutos para milissegundos
-    num_cortes = duracao_total_ms // duracao_corte_ms  # Usando divisão inteira para evitar cortes extras
+    num_cortes = duracao_total_ms // duracao_corte_ms
 
-    # Loop para criar os cortes
-    for i in range(num_cortes):
+    # Obter o nome base do arquivo sem a extensão
+    nome_base = os.path.splitext(os.path.basename(arquivo))[0]
+
+    # Criar uma pasta para os novos arquivos MP3
+    if not os.path.exists(nome_base):
+        os.makedirs(nome_base)
+
+    # Mensagem de início do processo
+    print(f"Iniciando o processo de corte do arquivo {arquivo}...")
+
+    # Loop para criar os cortes com barra de progresso
+    for i in tqdm(range(num_cortes), desc="Cortando áudio"):
         # Definir o ponto inicial e final do corte
         inicio = i * duracao_corte_ms
         fim = inicio + duracao_corte_ms
         parte = audio[inicio:fim]
-        parte.export(f"parte_{i + 1}.mp3", format="mp3")
+        # Salvar o arquivo na pasta criada
+        parte.export(os.path.join(nome_base, f"parte_{i + 1}.mp3"), format="mp3")
 
     # Verificar se há um resto no final e criar um arquivo adicional se necessário
     resto = duracao_total_ms % duracao_corte_ms
     if resto > 0:
         parte_resto = audio[-resto:]
-        parte_resto.export(f"parte_{num_cortes + 1}.mp3", format="mp3")
+        parte_resto.export(os.path.join(nome_base, f"parte_{num_cortes + 1}.mp3"), format="mp3")
+
+    # Mensagem de conclusão do trabalho
+    print("O trabalho foi concluído com sucesso!")
 
 
 if __name__ == '__main__':
